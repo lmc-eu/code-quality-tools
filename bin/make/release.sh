6bc39dfd81906d8ad2e44278344aedad41ee7694
literat <tomas@litera.me>
2021-08-10 10:49:40 +0200

    Chore: Deploy scripts for creating release using CI and release command

---
+++ bin/make/release.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -o errexit
+
+head=$(git rev-parse --abbrev-ref HEAD)
+
+printf "\n=====>\tCreating release branch...\n"
+
+git branch --force release
+git checkout release
+
+printf "\n=====>\tCreating release commit...\n"
+
+yarn lerna version
+
+printf "\n=====>\tPushing...\n"
+
+git push --tags --set-upstream origin release:release
+
+printf "\n=====>\tSwitching back to previous branch...\n"
+
+git checkout "${head}"
+
+printf "\n=====>\tDeleting local release branch...\n"
+
+git branch --delete release
+
+printf "\n=====>\tRelease in progress: https://github.com/lmc-eu/code-quality-tools/actions\n"
