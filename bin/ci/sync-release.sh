c625846e4a1fb075408833007c5bd0df06374bc6
Tomáš Litera <tomas.litera@lmc.eu>
2021-12-16 13:10:05 +0100

    Chore(publish): Sync release branch with main branch

--- bin/ci/sync-release.sh
+++ bin/ci/sync-release.sh
@@ -2,23 +2,24 @@
 
 set -o errexit
 
-printf "\n=====>\tFetching current master...\n"
+printf "\n=====>\tFetching current main...\n"
 
 # Ensure we are merging the release into current branch tips to avoid pushes being rejected
-git fetch --no-tags --depth=50 origin master
+git fetch --no-tags --depth=50 origin main
 
-printf "\n=====>\tMerging to master...\n"
+printf "\n=====>\tMerging to main...\n"
 
-git checkout master
-git merge release
+git rebase main
+git checkout main
+git merge --ff-only release
 
 # Print the repo status after merging, for troubleshooting purposes
 git status
 git log --oneline HEAD~10..HEAD
 
-printf "\n=====>\tPushing master...\n"
+printf "\n=====>\tPushing main...\n"
 
-git push origin master
+git push origin main
 
 printf "\n=====>\tDeleting release branch...\n"
 git push origin :release
