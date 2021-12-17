236bd93f0f5c4b418599fb8e2ac49cda870dd812
Tomáš Litera <tomas.litera@lmc.eu>
2021-12-17 13:44:32 +0100

    Chore(repo): Rename commitlint config to use same dot notation as rest
      * configs for linters in this repo

---
+++ .commitlintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: ['./packages/commitlint-config'],
+  rules: {
+    'scope-enum': [
+      1,
+      'always',
+      [
+        // Use when commiting changes/additions/removals to exact package
+        'browserslist',
+        'commitlint',
+        'conventional-changelog',
+        'prettier',
+        'stylelint',
+        'eslint-config-base',
+        'eslint-config-lmc',
+        'eslint-config-graphql',
+        // Use when commiting changes/additions/removals to packages in context
+        'eslint',
+        'rule',
+        // Use when affecting CI process
+        'ci',
+        // Use for anything that does not directly affect rulesets, ie. updating repo-wide
+        'repo',
+      ],
+    ],
+
+    'footer-max-line-length': [0],
+  },
+  ignores: [(commit) => commit.includes('[CI-SKIP]'), (commit) => commit.includes('Pull request')],
+};
