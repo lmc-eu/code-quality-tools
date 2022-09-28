1347c5e439fef3392e67231f1b608d5a7d15f10d
Tomáš Litera <tomas.litera@lmc.eu>
2022-09-28 22:11:22 +0200

    Feat(conventional-changelog): Introduce recommended bump options
      * @see: https://github.com/conventional-changelog/conventional-changelog/pull/29

--- packages/conventional-changelog-lmc/src/index.js
+++ packages/conventional-changelog-lmc/src/index.js
@@ -1,7 +1,9 @@
 const parserOpts = require('./parser-opts');
 const writerOpts = require('./writer-opts');
+const recommendedBumpOpts = require('./bump-opts');
 
 module.exports = {
   parserOpts,
+  recommendedBumpOpts,
   writerOpts,
 };
