49bea0a81908076adb8690e43fc9bbcd732062f6
Tomáš Litera <tomas.litera@lmc.eu>
2022-10-03 09:27:31 +0200

    Fix(conventional-changelog): Output format of chnagelog main file

--- packages/conventional-changelog-lmc-bitbucket/src/index.js
+++ packages/conventional-changelog-lmc-bitbucket/src/index.js
@@ -1,21 +1,15 @@
-const Q = require('q');
-const readFile = Q.denodeify(require('fs').readFile);
-const { resolve } = require('path');
-const { parserOpts, writerOpts } = require('@lmc-eu/conventional-changelog-lmc');
+const Q = require(`q`);
+const conventionalChangelog = require(`./conventional-changelog`);
+const { parserOpts, writerOpts, recommendedBumpOpts } = require('@lmc-eu/conventional-changelog-lmc');
 
-module.exports = Q.all([
-  readFile(resolve(__dirname, 'templates/template.hbs'), 'utf-8'),
-  readFile(resolve(__dirname, 'templates/header.hbs'), 'utf-8'),
-  readFile(resolve(__dirname, 'templates/commit.hbs'), 'utf-8'),
-  readFile(resolve(__dirname, 'templates/footer.hbs'), 'utf-8'),
-]).spread((template, header, commit, footer) => {
-  writerOpts.mainTemplate = template;
-  writerOpts.headerPartial = header;
-  writerOpts.commitPartial = commit;
-  writerOpts.footerPartial = footer;
-
-  return {
+module.exports = Q.all([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts]).spread(
+  // Using same configuration as other configurations
+  // Did not find any documentation whether the output must be in this format
+  // eslint-disable-next-line no-shadow
+  (conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts) => ({
+    conventionalChangelog,
     parserOpts,
+    recommendedBumpOpts,
     writerOpts,
-  };
-});
+  }),
+);
--- packages/conventional-changelog-lmc-github/src/index.js
+++ packages/conventional-changelog-lmc-github/src/index.js
@@ -3,5 +3,13 @@ const conventionalChangelog = require(`./conventional-changelog`);
 const { parserOpts, writerOpts, recommendedBumpOpts } = require('@lmc-eu/conventional-changelog-lmc');
 
 module.exports = Q.all([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts]).spread(
-  (changelog, parser, bumper, writer) => ({ changelog, parser, recommendedBumpOpts, writer }),
+  // Using same configuration as other configurations
+  // Did not find any documentation whether the output must be in this format
+  // eslint-disable-next-line no-shadow
+  (conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts) => ({
+    conventionalChangelog,
+    parserOpts,
+    recommendedBumpOpts,
+    writerOpts,
+  }),
 );
