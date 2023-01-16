7c8432804bfa44e0749cc2d9154ea238e2a641a1
Tomáš Litera <tomas.litera@lmc.eu>
2023-01-16 21:16:42 +0100

    Chore(repo): Introduce script for package graduation
      * make stable version from prerelease version

---
+++ bin/make/graduate.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -o errexit
+
+head=$(git rev-parse --abbrev-ref HEAD)
+
+printf "\n=====>\tCreating release branch...\n"
+
+git branch --force release
+git checkout release
+
+printf "\n=====>\tCreating release locally...\n"
+
+yarn lerna version --conventional-graduate
+
+printf "\n=====>\tSwitching back to previous branch...\n"
+
+git checkout "${head}"
+
+printf "\n=====>\tMerging release branch...\n"
+
+git merge release
+
+printf "\n=====>\tPushing...\n"
+
+git push --tags origin "${head}:${head}"
+
+printf "\n=====>\tReleased 🎉\n"
