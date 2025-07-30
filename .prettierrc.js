749934bcee5d63346faafcd5943db5157ddf3cb2
Klaudia Turcekova <36670346+Kothy@users.noreply.github.com>
2025-07-30 09:28:20 +0200

    Feat(eslint): Updates eslint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,1 +1,4 @@
-module.exports = require('./packages/prettier-config');
+// eslint-disable-next-line import/no-relative-packages
+import prettierConfig from './packages/prettier-config/index.js';
+
+export default prettierConfig;
