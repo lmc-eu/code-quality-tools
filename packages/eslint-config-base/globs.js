9587d1fa3eab3757b905375ed2af780b164669a7
Klaudia Turcekova <36670346+Kothy@users.noreply.github.com>
2025-07-30 09:29:54 +0200

    Feat(eslint-config-base): Updates eslint to v9 and adds flat config

--- packages/eslint-config-base/globs.js
+++ packages/eslint-config-base/globs.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   // Test files patterns
   tests: ['test/**', '**/*.test.*', '**/*.spec.*'],
 
--- packages/eslint-config-base/index.js
+++ packages/eslint-config-base/index.js
@@ -1,38 +1,54 @@
-const globs = require('./globs');
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+import importsPlugin from 'eslint-plugin-import';
+import bestPractices from './rules/best-practices.js';
+import errors from './rules/errors.js';
+import node from './rules/node.js';
+import style from './rules/style.js';
+import variables from './rules/variables.js';
+import es6 from './rules/es6.js';
+import importsRules from './rules/imports.js';
+import strict from './rules/strict.js';
+import globs from './globs.js';
 
-module.exports = {
-  plugins: ['import'],
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.mjs', '.cjs', '.js', '.json', '.node'],
+export default [
+  {
+    extends: compat.extends('eslint-config-airbnb-base'),
+
+    plugins: {
+      import: importsPlugin,
+    },
+
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.mjs', '.cjs', '.js', '.json', '.node'],
+        },
       },
+    },
+
+    rules: {},
+  },
+  bestPractices,
+  errors,
+  node,
+  style,
+  variables,
+  es6,
+  importsRules,
+  strict,
+  {
+    files: globs.configs,
+
+    rules: {
+      // Using process.env is encouraged in configuration files
+      'node/no-process-env': 'off',
     },
   },
-
-  extends: [
-    'eslint-config-airbnb-base',
-    './rules/best-practices',
-    './rules/errors',
-    './rules/node',
-    './rules/style',
-    './rules/variables',
-    './rules/es6',
-    './rules/imports',
-    './rules/strict',
-  ].map(require.resolve),
-
-  rules: {},
-
-  overrides: [
-    {
-      files: globs.configs,
-
-      rules: {
-        // Using process.env is encouraged in configuration files
-        'node/no-process-env': 'off',
-      },
-    },
-  ],
-};
+];
