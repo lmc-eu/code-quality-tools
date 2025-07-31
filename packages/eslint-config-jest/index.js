0c0f685bf11da75d745c50811c302fbc6dacda46
Klaudia Turcekova <36670346+Kothy@users.noreply.github.com>
2025-07-31 09:00:04 +0200

    Fix(eslint-config-jest): Fixes after CR

--- packages/eslint-config-jest/index.js
+++ packages/eslint-config-jest/index.js
@@ -1,12 +1,9 @@
-import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
 import globs from '@lmc-eu/eslint-config-base/globs.js';
 import jest from 'eslint-plugin-jest';
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
 });
 
 export default [
