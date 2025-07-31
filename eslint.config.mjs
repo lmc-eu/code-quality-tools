c7c4d5786a456f1a0ec60eec758ae682bbe51c83
Klaudia Turcekova <36670346+Kothy@users.noreply.github.com>
2025-07-31 09:07:13 +0200

    Fix(package): Fixes after CR

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,9 @@
 import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import base from './packages/eslint-config-base/index.js';
 import optional from './packages/eslint-config-base/optional.js';
 import jest from './packages/eslint-config-jest/index.js';
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
-
 export default defineConfig([
   ...base,
   ...optional,
