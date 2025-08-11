a652e7994f97b0e94ba382ee330a3488a0f0ab79
literat <tomas@litera.me>
2025-08-11 17:44:32 +0200

    deps(commitlint)!: upgrade to commitlint v19
    BREAKING CHANGE: the package is provided as an ESM only
    BREAKING CHANGE: abandon usage of the `@lmc-eu/conventional-changelog-lmc`
    BREAKING CHANGE: drop support of Node.js v16

--- packages/commitlint-config/index.js
+++ packages/commitlint-config/index.js
@@ -1,34 +1,13 @@
-module.exports = {
+import conventionalConfig from '@commitlint/config-conventional';
+
+export default {
   extends: ['@commitlint/config-conventional'],
-  parserPreset: '@lmc-eu/conventional-changelog-lmc',
   ignores: [(commit) => commit.includes('[CI-SKIP]')],
   rules: {
-    'type-case': [
-      1,
-      'always',
-      // We are using `pascal-case` for every type but
-      // allowing `upper-case` only for use of `BREAKING CHANGE(S)`
-      // so this should not lead to define any other type in `upper-case`
-      ['pascal-case', 'upper-case'],
-    ],
     'type-enum': [
       2,
       'always',
-      [
-        'Feat',
-        'Fix',
-        'Docs',
-        'Style',
-        'Refactor',
-        'Test',
-        'Revert',
-        'Perf',
-        'Chore',
-        'Deps',
-        'BREAKING CHANGES',
-        'BREAKING CHANGE',
-      ],
+      [...conventionalConfig.rules['type-enum'][2], 'deps', 'BREAKING CHANGES', 'BREAKING CHANGE'],
     ],
-    'subject-case': [2, 'always', 'sentence-case'],
   },
 };
