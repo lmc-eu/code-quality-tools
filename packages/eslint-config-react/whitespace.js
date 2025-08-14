ac2c0c93f379e8afb988e24b347138dd3d3849f0
literat <tomas@litera.me>
2025-08-14 09:40:52 +0200

    Fix(eslint-config-react): Remove unsupported `__dirname` from ESM
      * add missing name property for the whitespace config

--- packages/eslint-config-react/whitespace.js
+++ packages/eslint-config-react/whitespace.js
@@ -1,12 +1,11 @@
 import { FlatCompat } from '@eslint/eslintrc';
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat();
 
 export default [
+  ...compat.extends('eslint-config-airbnb/whitespace'),
   {
-    extends: compat.extends('eslint-config-airbnb/whitespace'),
+    name: '@alma-oss/eslint-config-react/whitespace',
     rules: {},
   },
 ];
