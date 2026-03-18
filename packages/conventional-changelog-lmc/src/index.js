e79fa29997c1caa68f5e22330f43a2352772909a
literat <tomas@litera.me>
2026-03-18 09:35:07 +0100

    feat(conventional-changelog)!: abandon `@lmc-eu/conventional-changelog-lmc`
      * switch to conventional commits instead
      * @see https://www.conventionalcommits.org/en/v1.0.0/

--- packages/conventional-changelog-lmc/src/index.js
+++
@@ -1,9 +0,0 @@
-const parserOpts = require('./parser-opts');
-const writerOpts = require('./writer-opts');
-const recommendedBumpOpts = require('./bump-opts');
-
-module.exports = {
-  parserOpts,
-  recommendedBumpOpts,
-  writerOpts,
-};
